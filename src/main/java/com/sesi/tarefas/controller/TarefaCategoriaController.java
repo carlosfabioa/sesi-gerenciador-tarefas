bdf50f3dc4e5814c5dc530611cc13405ba93e3cf
carlos fabio <carlosfabioa@hotmail.com>
2024-09-30 12:02:14 -0300

    tarefa categoria

--- src/main/java/com/sesi/tarefas/controller/TarefaCategoriaController.java
+++ src/main/java/com/sesi/tarefas/controller/TarefaCategoriaController.java
@@ -1,15 +1,46 @@
 package com.sesi.tarefas.controller;
 
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.sesi.tarefas.model.TarefaCategoria;
+import com.sesi.tarefas.repository.TarefaCategoriaRepository;
+
+import jakarta.websocket.server.PathParam;
 
 @Controller
 public class TarefaCategoriaController {
 
+	@Autowired
+	TarefaCategoriaRepository tarefaCategoriaRepository;
+	
 	@GetMapping("/listarCategoria")
-	public String listarCategoria() {
-		
+	public String listarCategoria(Model modelo) {
+		//modelo.addAttribute("categorias", tarefaCategoriaRepository.findAll());
 		return "listarTarefaCategoria";
+	}
+	
+	@GetMapping("editarCategoria/{id}")
+	public String editarCategoria(@PathVariable("id") int id, Model modelo) {
+		Optional<TarefaCategoria> categoriaOpt = tarefaCategoriaRepository.findById(id);
+		
+		if(categoriaOpt.isPresent()) {
+			modelo.addAttribute("categoria", categoriaOpt.get());
+			return "formularioTarefaCategoria";
+		}else {
+			return "redirect:/listarTarefaCategoria";
+		}
+	}
+	
+	@GetMapping("/formularioTarefaCategoria")
+	public String mostrarFormulario() {
+		return "formularioTarefaCategoria";
 	}
 	
 }
